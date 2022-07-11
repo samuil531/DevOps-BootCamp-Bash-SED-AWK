f1452e1550aa61723f74246da736ee943eea4060
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-11 22:59:35 +0300

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-# Put you code here
+awk 'BEGIN {FS = OFS = ":"} {if ($1 == "saned") $7 = "/bin/bash"} {print $0}' passwd |
+sed '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/g' |
+awk 'BEGIN {FS = OFS = ":"} {print $1,$3,$5,$7}' |
+sed '/daemon/d' |
+awk 'BEGIN {FS = OFS = ":"} {if ($2 % 2 == 0) $4 = "/bin/zsh"} {print $0}' > passwd_new 
